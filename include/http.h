1a73ff8682ef53a2522c4530f46ae554f901ee37
Luc Wallace <144689311+luc-wallace@users.noreply.github.com>
2025-01-25 16:43:41 +0000

    Define HTTP util types

--- include/http.h
+++ include/http.h
@@ -1,7 +1,44 @@
 #ifndef http_h
 #define http_h
 
-// TODO: add request parameters here
-struct Request {};
+typedef enum HttpMethod {
+  HTTP_GET,
+  HTTP_HEAD,
+  HTTP_POST,
+  HTTP_PUT,
+  HTTP_DELETE,
+  HTTP_CONNECT,
+  HTTP_OPTIONS,
+  HTTP_TRACE,
+  HTTP_PATCH
+} HttpMethod;
+
+typedef struct HttpHeader {
+  const char* name;
+  const char* value;
+} HttpHeader;
+
+typedef struct HttpRequest {
+  HttpMethod method;
+  const char* path;
+  const char* body;
+} HttpRequest;
+
+typedef enum HttpStatusCode {
+  HTTP_OK = 200,
+  HTTP_CREATED = 201,
+  HTTP_BAD_REQUEST = 400,
+  HTTP_NOT_FOUND = 404,
+  HTTP_INTERNAL_SERVER_ERROR = 500
+  // TODO: add all status codes
+} HttpStatusCode;
+
+typedef struct HttpResponse {
+  HttpStatusCode status_code;
+  HttpHeader* headers;
+  size_t header_count;
+
+  char* body;
+} HttpResponse;
 
 #endif
